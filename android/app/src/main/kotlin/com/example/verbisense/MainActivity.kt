02ba1a09c764e7ddf84b787c9b45a4e0e0f0a3bd
Jagadeeswaran27 <jagadeeswaran2705@gmail.com>
2025-09-14 22:38:35 +0530

    removed all the files

--- android/app/src/main/kotlin/com/example/verbisense/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.verbisense
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/resources/icons.dart
+++
@@ -1,6 +0,0 @@
-class Icons {
-  Icons._();
-
-  static const String document = 'assets/icons/document.svg';
-  static const String google = 'assets/icons/google.svg';
-}
